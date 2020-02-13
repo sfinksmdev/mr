627b7fcfc485feea54acddf95c4aff0c50b75cb7
sfinksmdev <50462547+sfinksmdev@users.noreply.github.com>
2020-02-13 13:10:18 +0100

    Add files via upload

--- js/faq.js
+++ js/faq.js
@@ -28,3 +28,36 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var str;
+if(navigator.language.substring(0,2) == "es"){
+  str="ss";
+}
+else{
+  str="s";
+}
+var img = [document.getElementById(str+"1")];
+var modalImg = document.getElementById("ms");
+var captionText = document.getElementById("caption");
+img.push(document.getElementById(str+"2"));
+
+
+for (i = 0; i < 2; i++) {
+  img[i].onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("modal")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
